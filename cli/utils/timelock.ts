36abdc45aa88d03682ab0746c30fc24488dc7346
0xSkly <skly.beets@protonmail.com>
2021-10-21 09:58:47 +0200

    increase additional delay to 20min

--- cli/utils/timelock.ts
+++ cli/utils/timelock.ts
@@ -1,14 +1,14 @@
-import moment from "moment"
-import { network } from "hardhat"
-import { StoredTimelockTransaction } from "../types"
+import moment from "moment";
+import { network } from "hardhat";
+import { StoredTimelockTransaction } from "../types";
 
-const isMainnet = network.name === process.env.MAINNET
+const isMainnet = network.name === process.env.MAINNET;
 
 const storedTransactions: Record<
   string,
   StoredTimelockTransaction
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-> = require(`../../.timelock/transactions.${network.name}.json`)
+> = require(`../../.timelock/transactions.${network.name}.json`);
 
 export const timelockQueueQuestions = [
   {
@@ -27,23 +27,30 @@ export const timelockQueueQuestions = [
   {
     name: "eta",
     type: "number",
-    message: `eta when to be executed on timelock (default: ${isMainnet ? "6h + 10min" : "12mins"})`,
+    message: `eta when to be executed on timelock (default: ${
+      isMainnet ? "6h + 20min" : "12mins"
+    })`,
     when: (answers: any) => answers.timelock,
     default: isMainnet
       ? moment()
-          .add(6 * 60 + 10, "minutes")
+          .add(6 * 60 + 20, "minutes")
           .unix()
       : moment().add(12, "minutes").unix(),
   },
-]
+];
 
 export function getTimelockTransactionIds(onlyExecutable = true) {
   if (onlyExecutable) {
     return Object.keys(storedTransactions).filter((transactionId) => {
-      return !storedTransactions[transactionId].executed && moment().isSameOrAfter(moment.unix(storedTransactions[transactionId].eta))
-    })
+      return (
+        !storedTransactions[transactionId].executed &&
+        moment().isSameOrAfter(
+          moment.unix(storedTransactions[transactionId].eta)
+        )
+      );
+    });
   } else {
-    return Object.keys(storedTransactions)
+    return Object.keys(storedTransactions);
   }
 }
 
@@ -51,13 +58,17 @@ export function getTimelockTransactions() {
   return Object.keys(storedTransactions)
     .map(
       (transactionId) =>
-        `[${transactionId}][${moment.unix(storedTransactions[transactionId].eta)}]  - ${
-          storedTransactions[transactionId].targetContract.name
-        } - ${storedTransactions[transactionId].targetContract.address} - ${
+        `[${transactionId}][${moment.unix(
+          storedTransactions[transactionId].eta
+        )}]  - ${storedTransactions[transactionId].targetContract.name} - ${
+          storedTransactions[transactionId].targetContract.address
+        } - ${
           storedTransactions[transactionId].targetFunction.identifier
-        } - ${JSON.stringify(storedTransactions[transactionId].targetFunction.args)} - executed: ${storedTransactions[transactionId].executed} ${
+        } - ${JSON.stringify(
+          storedTransactions[transactionId].targetFunction.args
+        )} - executed: ${storedTransactions[transactionId].executed} ${
           storedTransactions[transactionId].executeTxHash ?? ""
         }`
     )
-    .join("\n")
+    .join("\n");
 }
